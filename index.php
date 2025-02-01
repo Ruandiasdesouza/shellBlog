9507376559a10258a28647bf623faeedde661611
Ruandiasdesouza <diasdesouzaruan@gmail.com>
2025-02-01 12:45:44 -0300

    inicio

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>Shel Blog</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <h3>Shell Blog</h3>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Início</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="linux.php">Linux</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="redes.php">Redes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="pessoal.php">Pessoal</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="sobre.php">Sobre</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+            </form>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="card-with-image">
+            <img src="terminal Linux com painel desfocado, sem o PC.png" alt="Imagem ao lado do card">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">NÃO SABE O QUE PROCURA?</h5>
+                    <p class="card-text">Para quem não sabe o que busca, todos os resultados são válidos.</p>
+                    <p class="card-text"> Clique aqui para ver todas as publicações recentes do blog</p>
+                    <a href="#" class="btn btn-primary">VER POSTS</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Codigo PHP -->
+    <?php
+
+    ?>
+
+</body>
+</html>
---
+++ linux.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>Shel Blog</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <h3>Shell Blog</h3>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Início</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="linux.php">Linux</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="redes.php">Redes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="pessoal.php">Pessoal</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="sobre.php">Sobre</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+            </form>
+        </div>
+    </nav>
+
+    
+    <!-- Codigo PHP -->
+    <?php
+
+    ?>
+
+</body>
+</html>
